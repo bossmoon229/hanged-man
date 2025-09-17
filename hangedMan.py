991ffa5707af8e793290080abbbb904f65142577
bossmoon229 <alinaignatich@gmail.com>
2025-09-17 11:10:39 +0300

    fix all bugs and done all logic in game

--- hangedMan.py
+++ hangedMan.py
@@ -1,48 +1,104 @@
 begin = input("Вітаю в грі повішений. Бажаєте почати? якщо так напишіть 'yes' ")
 
-countChar = 0
-
+wrondAnswer = []
 word = ['w','o','r','d']
 userWord = ['_', '_', '_', '_']
-hangedMan =[
-'+---+',
-'|   |',
-'    |',
-'    |',
-'    |',
-'    |',
-'=========',
+stages = [
+    [
+        "+---+",
+        " |  |",
+        "    |",
+        "    |",
+        "    |",
+        "    |",
+        "========="
+    ],
+    [
+        "+---+",
+        " |  |",
+        " O  |",
+        "    |",
+        "    |",
+        "    |",
+        "========="
+    ],
+    [
+        "+---+",
+        " |  |",
+        " O  |",
+        "|   |",
+        "    |",
+        "    |",
+        "========="
+    ],
+    [
+        "+---+",
+        " |  |",
+        " O  |",
+        "/|  |",
+        "    |",
+        "    |",
+        "========="
+    ],
+    [
+        "+---+",
+        " |  |",
+        " O  |",
+        "/|\\ |",
+        "    |",
+        "    |",
+        "========="
+    ],
+    [
+        "+---+",
+        " |  |",
+        " O  |",
+        "/|\\ |",
+        "/   |",
+        "    |",
+        "========="
+    ],
+    [
+        "+---+",
+        " |  |",
+        " O  |",
+        "/|\\ |",
+        "/ \\ |",
+        "    |",
+        "========="
+    ]
 ]
 
-# print(word.index('w'))
-print(word)
-# print(userWord)
-# for line in hangedMan:
-#     print(line)
+
+mistakes = -1
 
 if begin == 'yes':
     while True:
-        if countChar == len(userWord):
-            print('Ви виграли!')
+        if mistakes+1 == len(stages):
+            print('Ви програли :(')
             quit()
-
+        if '_' not in userWord:
+            print('Ви виграли!, Ваше слово ' + ''.join(userWord))
+            quit()
         userInput = input('Введіть літеру, яка на вашу думку може бути: ')
-        if(userInput == 'quit'):
+        if userInput == 'quit':
             quit()
-        if(len(userInput) > 1 or len(userInput) == ''):
+        if len(userInput) > 1 or len(userInput) == '':
             print('Введіть будь ласка тільки одну літеру')
             continue
-
+        if userInput in wrondAnswer:
+            print('Ви вже вводили цю літеру')
+            continue
+        if userInput not in word:
+            mistakes+=1
+            for line in stages[mistakes]:
+                print(line)
+            wrondAnswer.append(userInput)
         else:
             if userInput in word:
-
-                countChar += 1
-                print('Гарна робота, ви вгадали літету' + userInput)
+                print('Гарна робота, ви вгадали літету ' + userInput)
                 indexRemove = word.index(userInput)
                 userWord.pop(indexRemove)
-                print(userWord)
                 userWord.insert(word.index(userInput),userInput)
-                print(userWord)
-
-
-        # if userInput != chr(userInput)
+                print(''.join(userWord))
+                # print(userWord)
